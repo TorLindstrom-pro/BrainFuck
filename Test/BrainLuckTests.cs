2685c1e734ca603d3410ecd7738716cd776895ef
TorLindstrom <tor.lindstrom@winlas.se>
2025-03-19 14:07:23 +0100

    feat: echo single letter

--- Test/BrainLuckTests.cs
+++ Test/BrainLuckTests.cs
@@ -5,13 +5,13 @@ namespace Test;
 
 public class BrainLuckTests
 {
-    [Fact]
-    public void Test1()
+    [Fact(DisplayName = "Echo single letter")]
+    public void EchoSingleLetter()
     {
         // act
-        var result = Kata.BrainLuck("", "");
+        var result = Kata.BrainLuck(",.", "a");
         
         // assert
-        result.Should().Be("");
+        result.Should().Be("a");
     }
 }
